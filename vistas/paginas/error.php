80f3dcd30182fafa1f3a773469de85dd9d4a1859
Integradora <a3522110482@alumno.uttehuacan.edu.mx>
2023-10-06 22:58:26 -0600

    welcome4b

---
+++ vistas/paginas/error.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta name="author" content="NextGenerationDev">
+   <title>Wedding, Wedding Planer & Event HTML Template</title>
+   <link rel="shortcut icon" href="assets/img/favicon/favicon.png">
+   <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+   <link rel="stylesheet" href="assets/css/bootstrap.css.map">
+   <link rel="stylesheet" href="assets/css/owl.min.css">
+   <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+   <link rel="stylesheet" href="assets/css/all.min.css">
+   <link rel="stylesheet" href="assets/css/animate.css">
+   <link rel="stylesheet" href="assets/css/magnific-popup.css">
+   <link rel="stylesheet" href="assets/css/swiper-bundle.min.css">
+   <link rel="stylesheet" href="assets/css/main.css">
+</head>
+<body>
+
+<div class="scroll-up">
+   <svg class="scroll-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+      <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
+   </svg>
+</div>
+<section class="breadcumd__banner">
+   <div class="container">
+      <div class="breadcumd__wrapper center">
+         <h1 class="left__content">
+            error 404
+         </h1>
+         <ul class="right__content">
+            <li>
+               <a href="index.html">
+                  Home
+               </a>
+            </li>
+            <li>
+               <i class="fa-solid fa-chevron-right"></i>
+            </li>
+            <li>
+               404
+            </li>
+         </ul>
+      </div>
+   </div>
+</section>
+   <script src="assets/js/jquery-3.6.0.min.js"></script>
+   <script src="assets/js/bootstrap.bundle.min.js"></script>
+   <script src="assets/js/viewport.jquery.js"></script>
+   <script src="assets/js/wow.min.js"></script>
+   <script src="assets/js/owl.min.js"></script>
+   <script src="assets/js/swiper-bundle.min.js"></script> 
+   <script src="assets/js/jquery.magnific-popup.min.js"></script> 
+   <script src="assets/js/main.js"></script>
+</body>
+</html>
