fbff357212029524afec074c14fba3b1bc23d277
Integradora <a3522110482@alumno.uttehuacan.edu.mx>
2023-10-18 22:37:54 -0600

    egrf38

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 #index.php;
-#En el situaremos la salida de las vistas al usuario y tambien a traves
 #de el enviaremos los distintas acciones que el usuario envie al controlador
 
     require_once ("controladores/plantilla.controlador.php");
